29d0c6c57e40b4aa236e542ae50b2e1557b6fca3
AaronBuma <aaron.buma@gmail.com>
2025-06-14 10:01:31 -0700

    removing duplicate tf vars

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "website_url" {
-  value       = s3-bucket-website-aaronbuma.website.website_endpoint
-  description = "The public URL of the static website"
-}
-
-output "bucket_name" {
-  value       = s3-bucket-website-aaronbuma.website.bucket
-  description = "The name of the S3 bucket"
-}
